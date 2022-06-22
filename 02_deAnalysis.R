b0113ecc10c506a8e19c114228ca7bd084375969
Umar Niazi <uhkniazi@gmail.com>
2022-06-22 15:44:45 +0100

    DEG analysis with Limma completed. Continue with making figures and summaries of the results.

--- 02_deAnalysis.R
+++ 02_deAnalysis.R
@@ -58,186 +58,261 @@ dim(gse)
 mData = exprs(gse)
 dim(mData)
 plot(density(mData))
-rn = dfSample$description
+rn = as.character(dfSample$description)
 rn = strsplit(rn, ';')
 rn = sapply(rn, function(x) return(x[2]))
 identical(colnames(mData), rn)
 rownames(dfSample) = rn
 
-## select one tissue
-dfSample$description[1]
-table(dfSample$group2)
-i = which(dfSample$group2 == 'liver')
+## select one disease type
+iSub = which(dfSample$group1 %in% 'CLD-BA')
+dim(mData)
+mData = mData[,iSub]
+dfSample = dfSample[iSub,]
 
-dfSample = dfSample[i,]
-mData = mData[,i]
+dfSample = droplevels.data.frame(dfSample)
 identical(colnames(mData), rownames(dfSample))
 
-### set up data for modelling with stan
-mData.orig = mData
-#i = which(rownames(mData.orig) %in% c('16727967', '16681593', '16972339'))
-cvSel = '17006392'#scan(what=character())
-i = which(rownames(mData.orig) %in% cvSel)
-length(i)
-mData = (as.matrix(mData.orig[i,]))
+
+#' ### set up data for modelling with stan
+#' mData.orig = mData
+#' #i = which(rownames(mData.orig) %in% c('16727967', '16681593', '16972339'))
+#' cvSel = '17006392'#scan(what=character())
+#' i = which(rownames(mData.orig) %in% cvSel)
+#' length(i)
+#' mData = (as.matrix(mData.orig[i,]))
+#' dim(mData)
+#' dim(dfSample)
+#' 
+#' plot(density(mData))
+#' range(mData)
+#' 
+#' ## prepare covariates for model matrix
+#' dfData = data.frame((mData))
+#' dim(dfData); dim(dfSample);
+#' dfData = stack(dfData)
+#' dim(dfData)
+#' head(dfData)
+#' table(dfSample$group1); table(dfSample$group2); table(dfSample$group3)
+#' f = factor(dfSample$group1)
+#' levels(f)
+#' 
+#' dfData$fTreatment = f
+#' dfData$fGender = factor(dfSample$Gender)
+#' dfData$fEthnicity = factor(dfSample$Ethnicity)
+#' dfData$age = (dfSample$Age)
+#' dfData$Coef.1 = factor(dfData$fTreatment:dfData$ind)
+#' dfData$Coef.2 = factor(dfData$fGender:dfData$ind)
+#' dfData$Coef.3 = factor(dfData$fEthnicity:dfData$ind)
+#' dfData$Coef.4 = factor(dfData$ind)
+#' dim(dfData)
+#' dfData = droplevels.data.frame(dfData)
+#' #dfData = dfData[order(dfData$Coef, dfData$Coef.adj1, dfData$Coef.adj2), ]
+#' str(dfData)
+#' 
+#' # these db packages interfere with stan so unload
+#' detach('package:org.Hs.eg.db', unload=T)
+#' ## setup the stan model
+#' library(Rcpp)
+#' library(rstan)
+#' rstan_options(auto_write = TRUE)
+#' options(mc.cores = parallel::detectCores())
+#' 
+#' stanDso = rstan::stan_model(file='tResponse3RandomEffects1SlopeMultipleScales.stan')
+#' 
+#' 
+#' ### setup data object for stan and run model
+#' ## subset the data to get the second level of nested parameters
+#' ## this is done to avoid loops in the stan script to map the scale parameters
+#' ## of each ind/gene to the respective set of coefficients for jitters
+#' d = dfData[!duplicated(dfData$Coef.1), ]
+#' d2 = dfData[!duplicated(dfData$Coef.2), ]
+#' d3 = dfData[!duplicated(dfData$Coef.3), ]
+#' d4 = dfData[!duplicated(dfData$Coef.4), ]
+#' 
+#' lStanData = list(Ntotal=nrow(dfData),
+#'                  Nclusters1=nlevels(dfData$Coef.1),
+#'                  Nclusters2=nlevels(dfData$Coef.2),
+#'                  Nclusters3=nlevels(dfData$Coef.3),
+#'                  Nclusters4=nlevels(dfData$Coef.4),
+#'                  NScaleBatches1 = nlevels(dfData$ind), # to add a separate scale term for each gene
+#'                  NScaleBatches2 = nlevels(dfData$ind), # to add a separate scale term for each gene
+#'                  NScaleBatches3 = nlevels(dfData$ind), # to add a separate scale term for each gene
+#'                  NScaleBatches4 = nlevels(dfData$ind), # to add a separate scale term for each gene
+#'                  NgroupMap1=as.numeric(dfData$Coef.1),
+#'                  NgroupMap2=as.numeric(dfData$Coef.2),
+#'                  NgroupMap3=as.numeric(dfData$Coef.3),
+#'                  NgroupMap4=as.numeric(dfData$Coef.4),
+#'                  NBatchMap1=as.numeric(d$ind), # this is where we use the second level mapping
+#'                  NBatchMap2=as.numeric(d2$ind), # this is where we use the second level mapping
+#'                  NBatchMap3=as.numeric(d3$ind), # this is where we use the second level mapping
+#'                  NBatchMap4=as.numeric(d4$ind), # this is where we use the second level mapping
+#'                  Nnu=nlevels(dfData$ind),
+#'                  NsigmaPop=nlevels(dfData$ind),
+#'                  NnuMap=as.numeric(dfData$ind),
+#'                  NsigmaPopMap=as.numeric(dfData$ind),
+#'                  y=as.numeric(dfData$values),
+#'                  X = as.numeric(dfData$age),
+#'                  intercept = mean(dfData$values), intercept_sd= sd(dfData$values)*3)
+#' 
+#' initf = function(chain_id = 1) {
+#'   list(sigmaRan1 = rep(1, times=lStanData$NScaleBatches1),
+#'        #sigmaRan2= rep(0.1, times=lStanData$NScaleBatches2),
+#'        rGroupsJitter1 = rep(0, times=lStanData$Nclusters1))
+#'        #rGroupsJitter2_scaled = rep(0, times=lStanData$Nclusters2),
+#'        #phi_scaled=rep(15, times=lStanData$Nphi))
+#' }
+#' 
+#' 
+#' ptm = proc.time()
+#' 
+#' fit.stan = sampling(stanDso, data=lStanData, iter=2000, chains=2,
+#'                     pars=c('sigmaRan1',
+#'                            'sigmaRan2',
+#'                            'sigmaRan3',
+#'                            'nu',
+#'                            #'mu',
+#'                            'rGroupsJitter1',
+#'                            'rGroupsJitter2',
+#'                            'rGroupsJitter3',
+#'                            'rGroupsSlopes',
+#'                            'betas',
+#'                            'slope',
+#'                            'sigmaPop'
+#'                     ),
+#'                     cores=2)#, init=initf)#, control=list(adapt_delta=0.99, max_treedepth = 11), init=initf)
+#' #save(fit.stan, file='results/fit.stan.nb_3Mar.rds')
+#' ptm.end = proc.time()
+#' print(fit.stan)
+#' ## compare with lme4
+#' library(lme4)
+#' fit.lme = lmer(values ~ 1 + age + (1 | Coef.1) + (1 | Coef.2) + (1 | Coef.3), data=dfData)
+#' summary(fit.lme)
+
+# ## extract results
+# ## get the coefficient of interest - Coef.1 (treatment) in our case from the random coefficients section
+# mCoef = extract(fit.stan)$rGroupsJitter1
+# dim(mCoef)
+# # # ## get the intercept at population level
+# iIntercept = as.numeric(extract(fit.stan)$betas)
+# # ## add the intercept to each random effect variable, to get the full coefficient
+# mCoef = sweep(mCoef, 1, iIntercept, '+')
+# 
+# ## function to calculate statistics for differences between coefficients
+# getDifference = function(ivData, ivBaseline){
+#   stopifnot(length(ivData) == length(ivBaseline))
+#   # get the difference vector
+#   d = ivData - ivBaseline
+#   # get the z value
+#   z = mean(d)/sd(d)
+#   # get 2 sided p-value
+#   p = pnorm(-abs(mean(d)/sd(d)))*2
+#   return(list(z=z, p=p))
+# }
+# 
+# ## split the data into the comparisons required
+# d = data.frame(cols=1:ncol(mCoef), mods=levels(dfData$Coef))
+# # the split is done below on : symbol, but factor name has a : symbol due
+# # to creation of interaction earlier, do some acrobatics to sort that issue
+# ## split this factor into sub factors
+# f = strsplit(as.character(d$mods), ':')
+# d = cbind(d, do.call(rbind, f))
+# head(d)
+# # d$`1` = d$`1`:d$`2`
+# # d = d[,-4]
+# colnames(d) = c(colnames(d)[1:2], c('fBatch', 'ind'))
+# str(d)
+# d$split = factor(d$ind)
+# 
+# levels(d$fBatch)
+## deg analysis using limma
+library(limma)
+fTissue = dfSample$group2
+levels(fTissue)
+fTissue = relevel(fTissue, 'liver')
+fPID = dfSample$group3
+nlevels(fPID)
+
+design = model.matrix(~ fTissue + fPID, data=dfSample)
+head(design)
+
 dim(mData)
-dim(dfSample)
+dim(design)
+fit = lmFit(mData, design)
+fit = eBayes(fit)
+levels(fTissue)
 
-plot(density(mData))
-range(mData)
+dfLimmma.fat = topTable(fit, coef = 2, adjust='BH', number=Inf)
+table(dfLimmma.fat$adj.P.Val < 0.01)
 
-## prepare covariates for model matrix
-dfData = data.frame((mData))
-dim(dfData); dim(dfSample);
-dfData = stack(dfData)
-dim(dfData)
-head(dfData)
-table(dfSample$group1); table(dfSample$group2); table(dfSample$group3)
-f = factor(dfSample$group1)
-levels(f)
+dfLimmma.muscle = topTable(fit, coef = 3, adjust='BH', number=Inf)
+table(dfLimmma.muscle$adj.P.Val < 0.01)
 
-dfData$fTreatment = f
-dfData$fGender = factor(dfSample$Gender)
-dfData$fEthnicity = factor(dfSample$Ethnicity)
-dfData$age = (dfSample$Age)
-dfData$Coef.1 = factor(dfData$fTreatment:dfData$ind)
-dfData$Coef.2 = factor(dfData$fGender:dfData$ind)
-dfData$Coef.3 = factor(dfData$fEthnicity:dfData$ind)
-dfData$Coef.4 = factor(dfData$ind)
-dim(dfData)
-dfData = droplevels.data.frame(dfData)
-#dfData = dfData[order(dfData$Coef, dfData$Coef.adj1, dfData$Coef.adj2), ]
-str(dfData)
-
-# these db packages interfere with stan so unload 
-detach('package:org.Hs.eg.db', unload=T)
-## setup the stan model
-library(Rcpp)
-library(rstan)
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-
-stanDso = rstan::stan_model(file='tResponse3RandomEffects1SlopeMultipleScales.stan')
-
-
-### setup data object for stan and run model
-## subset the data to get the second level of nested parameters
-## this is done to avoid loops in the stan script to map the scale parameters
-## of each ind/gene to the respective set of coefficients for jitters
-d = dfData[!duplicated(dfData$Coef.1), ]
-d2 = dfData[!duplicated(dfData$Coef.2), ]
-d3 = dfData[!duplicated(dfData$Coef.3), ]
-d4 = dfData[!duplicated(dfData$Coef.4), ]
-
-lStanData = list(Ntotal=nrow(dfData), 
-                 Nclusters1=nlevels(dfData$Coef.1),
-                 Nclusters2=nlevels(dfData$Coef.2),
-                 Nclusters3=nlevels(dfData$Coef.3),
-                 Nclusters4=nlevels(dfData$Coef.4),
-                 NScaleBatches1 = nlevels(dfData$ind), # to add a separate scale term for each gene
-                 NScaleBatches2 = nlevels(dfData$ind), # to add a separate scale term for each gene
-                 NScaleBatches3 = nlevels(dfData$ind), # to add a separate scale term for each gene
-                 NScaleBatches4 = nlevels(dfData$ind), # to add a separate scale term for each gene
-                 NgroupMap1=as.numeric(dfData$Coef.1),
-                 NgroupMap2=as.numeric(dfData$Coef.2),
-                 NgroupMap3=as.numeric(dfData$Coef.3),
-                 NgroupMap4=as.numeric(dfData$Coef.4),
-                 NBatchMap1=as.numeric(d$ind), # this is where we use the second level mapping
-                 NBatchMap2=as.numeric(d2$ind), # this is where we use the second level mapping
-                 NBatchMap3=as.numeric(d3$ind), # this is where we use the second level mapping
-                 NBatchMap4=as.numeric(d4$ind), # this is where we use the second level mapping
-                 Nnu=nlevels(dfData$ind),
-                 NsigmaPop=nlevels(dfData$ind),
-                 NnuMap=as.numeric(dfData$ind),
-                 NsigmaPopMap=as.numeric(dfData$ind),
-                 y=as.numeric(dfData$values),
-                 X = as.numeric(dfData$age),
-                 intercept = mean(dfData$values), intercept_sd= sd(dfData$values)*3)
-
-initf = function(chain_id = 1) {
-  list(sigmaRan1 = rep(1, times=lStanData$NScaleBatches1),
-       #sigmaRan2= rep(0.1, times=lStanData$NScaleBatches2),
-       rGroupsJitter1 = rep(0, times=lStanData$Nclusters1))
-       #rGroupsJitter2_scaled = rep(0, times=lStanData$Nclusters2),
-       #phi_scaled=rep(15, times=lStanData$Nphi))
-}
-
-
-ptm = proc.time()
-
-fit.stan = sampling(stanDso, data=lStanData, iter=2000, chains=2,
-                    pars=c('sigmaRan1',
-                           'sigmaRan2',
-                           'sigmaRan3',
-                           'nu',
-                           #'mu',
-                           'rGroupsJitter1',
-                           'rGroupsJitter2',
-                           'rGroupsJitter3',
-                           'rGroupsSlopes',
-                           'betas',
-                           'slope',
-                           'sigmaPop'
-                    ),
-                    cores=2)#, init=initf)#, control=list(adapt_delta=0.99, max_treedepth = 11), init=initf)
-#save(fit.stan, file='results/fit.stan.nb_3Mar.rds')
-ptm.end = proc.time()
-print(fit.stan)
-## compare with lme4
-library(lme4)
-fit.lme = lmer(values ~ 1 + age + (1 | Coef.1) + (1 | Coef.2) + (1 | Coef.3), data=dfData)
-summary(fit.lme)
-
-## extract results
-## get the coefficient of interest - Coef.1 (treatment) in our case from the random coefficients section
-mCoef = extract(fit.stan)$rGroupsJitter1
-dim(mCoef)
-# # ## get the intercept at population level
-iIntercept = as.numeric(extract(fit.stan)$betas)
-# ## add the intercept to each random effect variable, to get the full coefficient
-mCoef = sweep(mCoef, 1, iIntercept, '+')
-
-## function to calculate statistics for differences between coefficients
-getDifference = function(ivData, ivBaseline){
-  stopifnot(length(ivData) == length(ivBaseline))
-  # get the difference vector
-  d = ivData - ivBaseline
-  # get the z value
-  z = mean(d)/sd(d)
-  # get 2 sided p-value
-  p = pnorm(-abs(mean(d)/sd(d)))*2
-  return(list(z=z, p=p))
-}
-
-## split the data into the comparisons required
-d = data.frame(cols=1:ncol(mCoef), mods=levels(dfData$Coef))
-# the split is done below on : symbol, but factor name has a : symbol due
-# to creation of interaction earlier, do some acrobatics to sort that issue
-## split this factor into sub factors
-f = strsplit(as.character(d$mods), ':')
-d = cbind(d, do.call(rbind, f))
-head(d)
-# d$`1` = d$`1`:d$`2`
-# d = d[,-4]
-colnames(d) = c(colnames(d)[1:2], c('fBatch', 'ind'))
-str(d)
-d$split = factor(d$ind)
-
-levels(d$fBatch)
-## repeat this for each comparison
-
-## get a p-value for each comparison
-l = tapply(d$cols, d$split, FUN = function(x, base='CN', deflection='CLD-BA') {
-  c = x
-  names(c) = as.character(d$fBatch[c])
-  dif = getDifference(ivData = mCoef[,c[deflection]], ivBaseline = mCoef[,c[base]])
-  r = data.frame(ind= as.character(d$ind[c[base]]), coef.base=mean(mCoef[,c[base]]), 
-                 coef.deflection=mean(mCoef[,c[deflection]]), zscore=dif$z, pvalue=dif$p)
-  r$difference = r$coef.deflection - r$coef.base
-  #return(format(r, digi=3))
-  return(r)
-})
-
-dfResults = do.call(rbind, l)
+# ## repeat this for each comparison
+# 
+# ## get a p-value for each comparison
+# l = tapply(d$cols, d$split, FUN = function(x, base='CN', deflection='CLD-BA') {
+#   c = x
+#   names(c) = as.character(d$fBatch[c])
+#   dif = getDifference(ivData = mCoef[,c[deflection]], ivBaseline = mCoef[,c[base]])
+#   r = data.frame(ind= as.character(d$ind[c[base]]), coef.base=mean(mCoef[,c[base]]), 
+#                  coef.deflection=mean(mCoef[,c[deflection]]), zscore=dif$z, pvalue=dif$p)
+#   r$difference = r$coef.deflection - r$coef.base
+#   #return(format(r, digi=3))
+#   return(r)
+# })
+# 
+# dfResults = do.call(rbind, l)
 #dfResults$adj.P.Val = p.adjust(dfResults$pvalue, method='BH')
 
+## add annotations
+identical(rownames(dfLimmma.fat), rownames(dfLimmma.muscle))
+
+df = dfAnnotation[dfAnnotation$ID %in% rownames(dfLimmma.fat), ]
+dim(df)
+i = match(rownames(dfLimmma.fat), df$ID)
+df = df[i,]
+identical(rownames(dfLimmma.fat), as.character(df$ID))
+dfLimmma.fat$REFSEQ=as.character(df$GB_ACC)
+## get other related information
+df = select(x, keys = dfLimmma.fat$REFSEQ, columns = c('ENTREZID', 'SYMBOL'),
+            keytype = 'REFSEQ')
+identical(as.character(dfLimmma.fat$REFSEQ), as.character(df$REFSEQ))
+head(df)
+dfLimmma.fat = cbind(dfLimmma.fat, df[,c(2,3)]) 
+head(dfLimmma.fat)
+
+## second tissue
+identical(rownames(dfLimmma.muscle), rownames(dfLimmma.muscle))
+
+df = dfAnnotation[dfAnnotation$ID %in% rownames(dfLimmma.muscle), ]
+dim(df)
+i = match(rownames(dfLimmma.muscle), df$ID)
+df = df[i,]
+identical(rownames(dfLimmma.muscle), as.character(df$ID))
+dfLimmma.muscle$REFSEQ=as.character(df$GB_ACC)
+## get other related information
+df = select(x, keys = dfLimmma.muscle$REFSEQ, columns = c('ENTREZID', 'SYMBOL'),
+            keytype = 'REFSEQ')
+identical(as.character(dfLimmma.muscle$REFSEQ), as.character(df$REFSEQ))
+head(df)
+dfLimmma.muscle = cbind(dfLimmma.muscle, df[,c(2,3)]) 
+head(dfLimmma.muscle)
+
+# put both tables in same order
+table(rownames(dfLimmma.fat) %in% rownames(dfLimmma.muscle))
+dfLimmma.fat = dfLimmma.fat[rownames(dfLimmma.muscle),]
+identical(rownames(dfLimmma.fat), rownames(dfLimmma.muscle))
+
+## produce the plots 
+table(dfLimmma.fat$adj.P.Val < 0.01)
+range(dfLimmma.fat$logFC)
+f_plotVolcano(dfLimmma.fat, 'Fat vs Liver', p.adj.cut = 0.01, fc.lim = range(dfLimmma.fat$logFC) )#, fc.lim=c(-2.5, 2.5))
+f_plotVolcano(dfLimmma.muscle, 'Muscle vs Liver', p.adj.cut = 0.01, fc.lim = range(dfLimmma.muscle$logFC) )#, fc.lim=c(-2.5, 2.5))
+
+plotMeanFC(dfLimmma.fat$AveExpr, dfLimmma.fat, 0.01, 'Fat vs Liver')
+plotMeanFC(dfLimmma.muscle$AveExpr, dfLimmma.fat, 0.01, 'Muscle vs Liver')
+
+## save the results 
+write.csv(dfLimmma.fat, file='results/DEAnalysis_FatVSLiver.xls')
+write.csv(dfLimmma.muscle, file='results/DEAnalysis_MuscleVSLiver.xls')
